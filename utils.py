580c645c76cb817af8339ab73e6dcb13c9f0bb9b
Jorgedavyd <jorged.encyso@gmail.com>
2024-10-06 12:21:36 -0300

    last commit

--- utils.py
+++ utils.py
@@ -11,10 +11,10 @@ from matplotlib import cm
 from scipy.signal import stft
 
 def get_metadata() -> pd.DataFrame:
-    return pd.read_csv("data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv")
+    return pd.read_csv("../data/lunar/training/catalogs/apollo12_catalog_GradeA_final.csv")
 
 def preprocess_data_sample(sample_ind: int, metadata) -> Tuple[pd.DataFrame, float]:
-    df: pd.DataFrame = pd.read_csv(f"data/lunar/training/data/S12_GradeA/{metadata['filename'].iloc[sample_ind]}.csv", parse_dates =['time_abs(%Y-%m-%dT%H:%M:%S.%f)'] ,index_col = ['time_abs(%Y-%m-%dT%H:%M:%S.%f)'])
+    df: pd.DataFrame = pd.read_csv(f"../data/lunar/training/data/S12_GradeA/{metadata['filename'].iloc[sample_ind]}.csv", parse_dates =['time_abs(%Y-%m-%dT%H:%M:%S.%f)'] ,index_col = ['time_abs(%Y-%m-%dT%H:%M:%S.%f)'])
     df.columns = ['time', 'v']
     return df, metadata['time_rel(sec)'].iloc[sample_ind]
 
